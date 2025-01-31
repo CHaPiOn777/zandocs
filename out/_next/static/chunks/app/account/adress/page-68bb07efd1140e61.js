213a257e9884e6a423173ce75103a3586293a0bd
VladislavSh <vladislavshishkin123@gmail.com>
2025-01-31 13:20:07 +0300

    export

---
+++ out/_next/static/chunks/app/account/adress/page-68bb07efd1140e61.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[921],{84821:(e,s,r)=>{Promise.resolve().then(r.bind(r,79580))},79580:(e,s,r)=>{"use strict";r.r(s),r.d(s,{default:()=>_});var l=r(95155);r(12115);let _=()=>(0,l.jsx)("div",{children:"Платежный адрес"})}},e=>{var s=s=>e(e.s=s);e.O(0,[441,517,358],()=>s(84821)),_N_E=e.O()}]);
---
+++ out/_next/static/chunks/app/account/docs/page-a75d9087b9ef3457.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[772],{12808:(e,s,r)=>{Promise.resolve().then(r.bind(r,71793))},71793:(e,s,r)=>{"use strict";r.r(s),r.d(s,{default:()=>_});var l=r(95155);r(12115);let _=()=>(0,l.jsx)("div",{children:"Мои документы"})}},e=>{var s=s=>e(e.s=s);e.O(0,[441,517,358],()=>s(12808)),_N_E=e.O()}]);
