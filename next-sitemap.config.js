770f7574df8f999a72fef8005d5efef319b9b2a7
VladislavSh <vladislavshishkin123@gmail.com>
2025-02-14 15:12:11 +0300

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://zandocs.kz",
+  generateRobotsTxt: true, // Автоматически сгенерирует robots.txt
+  // Дополнительные настройки
+};
